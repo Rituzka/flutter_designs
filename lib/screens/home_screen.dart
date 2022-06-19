c6d58d2938281d1f814557e62c1a9f4760054d2e
Rita Casiello <rituzka@gmail.com>
2022-06-19 14:16:39 +0200

    background widget extract

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_designs/screens/scroll_design.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
+      body: Stack(
+        children: [
+          Background(),
+        ],
       ),
     );
   }
